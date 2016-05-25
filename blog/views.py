ac38d53a94dab594f8af6520c2e246492d167837
rajaraddi <rajaraddi@gmail.com>
2016-05-25 10:07:21 +0530

    Modified templates to read from database

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.contrib.auth.models import User
+from .models import Post
+
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html',{})
+    me=User.objects.get(username='admin')
+    #posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('created_date')
+    posts=Post.objects.filter(author=me)
+    return render(request, 'blog/post_list.html',{'posts':posts})
